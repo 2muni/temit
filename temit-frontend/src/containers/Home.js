0f7d897c6da25fee60ef793d3afc019cbf23d72d
2muni <relesinc@gmail.com>
2018-10-16 23:47:28 +0900

    Home layout #1 2018-10-16

---
+++ temit-frontend/src/containers/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return(
+      <>
+        <aside className="main-aside">
+
+        </aside>
+        <div className="postcard-list">
+          <div className="postcard"></div>
+          <div className="postcard"></div>
+          <div className="postcard"></div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Home;
--- temit-frontend/src/index.js
+++ temit-frontend/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/App.scss';
-import App from './containers/App';
+import { App, Home } from './containers';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+ReactDOM.render(
+  <Router>
+    <>
+      <Route path='/' component={App}/>
+      <div className="content-wrapper">
+        <Route exact path='/' component={Home}/>
+      </div>
+    </>
+  </Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
