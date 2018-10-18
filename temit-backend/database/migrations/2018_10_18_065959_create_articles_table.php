3cded487c00715bceea3cba3efb772c157c6399a
2muni <relesinc@gmail.com>
2018-10-18 20:54:00 +0900

    Rest API #1 2018-10-18

---
+++ temit-backend/database/migrations/2018_10_18_065959_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->string('thumbnail')->nullable();
+            $table->integer('tags')->nullable();
+            $table->string('url_slug');
+            $table->integer('like')->default(0);
+            $table->integer('comments')->default(0);
+            $table->boolean('is_temp')->default(false);
+            $table->string('user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
