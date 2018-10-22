d40ad42eacb325e18da0b10552a6124443f6474f
2muni <relesinc@gmail.com>
2018-10-22 19:56:40 +0900

    test

--- temit-backend/routes/web.php
+++ temit-backend/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('index');
+
+Route::get('/{path?}', function () {
+    return view('index');
+})->where('path', 'login');
 
 Auth::routes();
