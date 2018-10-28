43a7cccc03feda02247d22cc3b0a15a956ea178c
2muni <relesinc@gmail.com>
2018-10-28 15:38:40 +0900

    styled Article page #2 2018-10-28

--- resources/js/components/Post.js
+++ resources/js/components/Post.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import marked from 'marked';
-import { Icon } from 'react-materialize';
+import { Icon, Button } from 'react-materialize';
 
 import { dateSplit } from '../lib/tool'
+
+
+const handleHeight = (e) => {
+  e.target.style.height = '120px';
+  e.target.style.height = (e.target.scrollHeight)+"px";
+}
+const handleFocus = (e) => {
+  e.target.style.minHeight = '120px';
+  e.target.rows = '4';
+}
+const handleBlur = (e) => {
+  if(e.target.value) {
+    e.target.style.minHeight = '120px';
+    e.target.rows = '4';
+  }
+  else{
+    e.target.style.height = '53px';
+    e.target.style.minHeight = '0';
+    e.target.rows = '1';
+  }
+}
 
 const Post = ({ article, user, handleRemove }) => (
   <React.Fragment>
@@ -37,9 +58,46 @@ const Post = ({ article, user, handleRemove }) => (
     <img className="article-thumbnail" src={article.thumbnail}/>
     <div className="article-content" dangerouslySetInnerHTML={{__html: marked(article.body)}}/>
     <div className="article-tags">
+      <a href="#">teg 1</a>
+      <a href="#">teg 1</a>
+      <a href="#">teg 1</a>
+      <a href="#">teg 1</a>
+      <a href="#">teg 1</a>
     </div>
     <div className="article-comments">
-      {article.comments}
+      <div>24개의 댓글</div>
+      <textarea rows="1" style={{ height: '53px' }}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onKeyUp={handleHeight}/>
+      <div className="btn-wrapepr"><Button>댓글 작성</Button></div>
+      <div className="comment-list">
+
+        <div className="comment">
+          <div className="head">
+            <a href="#"><img className="circle" style={{backgroundColor: '#000'}}></img></a>
+            <div className="name-and-date">
+              <a href="#">username</a>
+              <div>date</div>
+            </div>
+          </div>
+          <div className="body">comment body</div>
+          <span className="replies-button">+ 답글 달기</span>
+        </div>
+
+        <div className="comment">
+          <div className="head">
+            <a href="#"><img className="circle" style={{backgroundColor: '#000'}}></img></a>
+            <div className="name-and-date">
+              <a href="#">username</a>
+              <div>date</div>
+            </div>
+          </div>
+          <div className="body">comment body</div>
+          <span className="replies-button">+ 답글 달기</span>
+        </div>
+
+      </div>
     </div>
   </React.Fragment>
 )
